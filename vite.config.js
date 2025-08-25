2b0d2ae5632e8d9883eb49a62becd55f7947e047
Noman Ali <nomi4698dg@gmail.com>
2025-08-26 00:07:54 +0500

    hello

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/': {
+      '/api': {
         target: 'http://backend-shopping-hub-production.up.railway.app',
         changeOrigin: true,
         secure: false
